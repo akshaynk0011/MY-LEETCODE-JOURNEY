430acc009833a7a856f296b14f0176dacadce5c2
NK NANAIAH <akshaynk0011@gmail.com>
2026-07-04 13:00:17 +0530

    Time: 36 ms (32.46%), Space: 159.3 MB (81.58%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+
+    List<int[]>[] graph;
+    boolean[] visited;
+    int answer = Integer.MAX_VALUE;
+
+    public int minScore(int n, int[][] roads) {
+
+        graph = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int d = road[2];
+
+            graph[u].add(new int[]{v, d});
+            graph[v].add(new int[]{u, d});
+        }
+
+        visited = new boolean[n + 1];
+
+        dfs(1);
+
+        return answer;
+    }
+
+    private void dfs(int city) {
+
+        visited[city] = true;
+
+        for (int[] next : graph[city]) {
+
+            int neighbour = next[0];
+            int distance = next[1];
+
+            answer = Math.min(answer, distance);
+
+            if (!visited[neighbour]) {
+                dfs(neighbour);
+            }
+        }
+    }
+}
