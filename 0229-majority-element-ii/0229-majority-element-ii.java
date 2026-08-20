52f9d885fd6b0e42c86a190625726abc5a26e9fc
NK NANAIAH <akshaynk0011@gmail.com>
2026-08-20 14:59:01 +0530

    Time: 2 ms (99.96%), Space: 52.9 MB (19.26%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,81 +4,52 @@ class Solution {
 
     public List<Integer> majorityElement(int[] nums) {
 
-        int candidate1 = 0;
-        int candidate2 = 0;
+        int el1 = 0, el2 = 1;
+        int cnt1 = 0, cnt2 = 0;
 
-        int count1 = 0;
-        int count2 = 0;
-
-        // Step 1: Find possible candidates
+        // Find two possible candidates
         for (int i = 0; i < nums.length; i++) {
 
-            // If candidate1 has no votes,
-            // take current number as candidate1.
-            // But make sure it is not already candidate2.
-            if (count1 == 0 && nums[i] != candidate2) {
-
-                candidate1 = nums[i];
-                count1 = 1;
-            }
-
-            // If candidate2 has no votes,
-            // take current number as candidate2.
-            // But make sure it is not already candidate1.
-            else if (count2 == 0 && nums[i] != candidate1) {
-
-                candidate2 = nums[i];
-                count2 = 1;
-            }
-
-            // Current number matches candidate1.
-            else if (nums[i] == candidate1) {
-
-                count1++;
-            }
-
-            // Current number matches candidate2.
-            else if (nums[i] == candidate2) {
-
-                count2++;
-            }
-
-            // Different from both candidates.
+            if (nums[i] == el1) {
+                cnt1++;
+            } 
+            else if (nums[i] == el2) {
+                cnt2++;
+            } 
+            else if (cnt1 == 0) {
+                el1 = nums[i];
+                cnt1 = 1;
+            } 
+            else if (cnt2 == 0) {
+                el2 = nums[i];
+                cnt2 = 1;
+            } 
             else {
-
-                count1--;
-                count2--;
+                cnt1--;
+                cnt2--;
             }
         }
 
-        // Step 2: Verify actual frequencies
-        count1 = 0;
-        count2 = 0;
+        // Verify candidates
+        cnt1 = 0;
+        cnt2 = 0;
 
         for (int i = 0; i < nums.length; i++) {
 
-            if (nums[i] == candidate1) {
-                count1++;
-            }
-
-            if (nums[i] == candidate2) {
-                count2++;
-            }
+            if (nums[i] == el1) cnt1++;
+            if (nums[i] == el2) cnt2++;
         }
 
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
 
-        if (count1 > nums.length / 3) {
-            answer.add(candidate1);
+        if (cnt1 > nums.length / 3) {
+            ans.add(el1);
         }
 
-        // Make sure we don't add the same candidate twice
-        if (candidate2 != candidate1 &&
-            count2 > nums.length / 3) {
-
-            answer.add(candidate2);
+        if (cnt2 > nums.length / 3 && el1 != el2) {
+            ans.add(el2);
         }
 
-        return answer;
+        return ans;
     }
 }
