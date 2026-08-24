8b0e97b8fb2315af153a603ed79f6bd8eb92f5a7
NK NANAIAH <akshaynk0011@gmail.com>
2026-08-24 20:38:52 +0530

    Time: 7 ms (99.06%), Space: 49.2 MB (27.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,38 +1,28 @@
-import java.util.*;
-
 class Solution {
 
     public int[][] merge(int[][] intervals) {
 
-        // Sort intervals by starting value
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<>();
 
-        List<int[]> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        // First interval
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        int[] current = intervals[0];
 
         for (int i = 1; i < intervals.length; i++) {
 
-            // Overlapping intervals
-            if (intervals[i][0] <= end) {
+            if (intervals[i][0] <= current[1]) {
 
-                end = Math.max(end, intervals[i][1]);
+                current[1] = Math.max(current[1], intervals[i][1]);
 
             } else {
 
-                // No overlap, store previous interval
-                ans.add(new int[]{start, end});
-
-                start = intervals[i][0];
-                end = intervals[i][1];
+                result.add(current);
+                current = intervals[i];
             }
         }
 
-        // Add the last interval
-        ans.add(new int[]{start, end});
+        result.add(current);
 
-        return ans.toArray(new int[ans.size()][]);
+        return result.toArray(new int[result.size()][]);
     }
 }
