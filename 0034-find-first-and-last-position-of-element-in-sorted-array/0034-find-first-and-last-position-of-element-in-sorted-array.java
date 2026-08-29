c695805f394a9c88c9853f3a6ba6a8b5b0064921
NK NANAIAH <akshaynk0011@gmail.com>
2026-08-29 20:50:40 +0530

    Time: 0 ms (100%), Space: 48.1 MB (71.76%) - LeetHub - Time: O(log n), Space: O(1)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,64 +1,50 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
 
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
+        int first = -1;
+        int last = -1;
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        // Find first occurrence
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        low = 0;
+        high = nums.length - 1;
+
+        // Find last occurrence
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
 
         return new int[]{first, last};
-    }
-
-    private int findFirst(int[] nums, int target) {
-
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                high = mid - 1;
-            }
-
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-
-            else {
-                high = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
-    private int findLast(int[] nums, int target) {
-
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                low = mid + 1;
-            }
-
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-
-            else {
-                high = mid - 1;
-            }
-        }
-
-        return ans;
     }
 }
 
