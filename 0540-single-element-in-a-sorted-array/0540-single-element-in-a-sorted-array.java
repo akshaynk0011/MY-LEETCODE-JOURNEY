817280ec35f55e0603657677e0bdb876e0fa780f
NK NANAIAH <akshaynk0011@gmail.com>
2026-08-31 22:01:44 +0530

    Time: 0 ms (100%), Space: 52.6 MB (95.37%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            // Make mid even
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
