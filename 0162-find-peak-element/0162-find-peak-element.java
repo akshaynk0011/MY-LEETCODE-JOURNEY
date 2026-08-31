62ad3270abfc6bb0cc7b246b7206472a4186383c
NK NANAIAH <akshaynk0011@gmail.com>
2026-08-31 22:01:50 +0530

    Time: 0 ms (100%), Space: 42.6 MB (99.97%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
